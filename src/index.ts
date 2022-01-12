328d4b0271ccb3c3d44c1dd23159618bf4eb10e1
Sergio-Al <54541807+Sergio-Al@users.noreply.github.com>
2022-01-12 13:02:07 -0400

    Adding apollo boost
    we need to configure our apollo bost.
    Since apollo uses fetch api, and that is not available on node js we use cross-fetch/polyfill.
    for configure our environment variables we use dotenv.
    all is working as expected

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
-import { myOtherNumber } from "./other";
-console.log(
-  "Hello, this is your boilerplate code for Node + Typescript + ESLint",
-  myOtherNumber
-);
+import 'cross-fetch/polyfill';
+import ApolloClient from "apollo-boost";
+import { config } from "dotenv";
+
+config(); // config my env variables
+
+const client = new ApolloClient({
+  uri: "https://api.github.com/graphql",
+  request: (operation) => {
+    operation.setContext({
+      headers: {
+        authorization: `Bearer ${process.env.PERSONAL_ACCESS_TOKEN_GITHUB}`,
+      },
+    });
+  },
+});
